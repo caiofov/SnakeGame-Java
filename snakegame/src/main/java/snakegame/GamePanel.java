9e701d41fad69334f296f30ac9529eb5ea1152d4
caiofov <cfoviana@gmail.com>
2021-10-06 12:30:17 -0300

    drawing score and game over

--- snakegame/src/main/java/snakegame/GamePanel.java
+++ snakegame/src/main/java/snakegame/GamePanel.java
@@ -41,36 +41,48 @@
         draw(g);
     }
     public void draw(Graphics g) {
-        g.setColor(Color.red);
-        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
         
-        //drawing snake body parts
-        for (int i =0 ; i< bodyParts; i++) {
-        	if (i==0) { //snake head
-        		g.setColor(Color.green);
-        		g.fillRect(x[i],y[i], UNIT_SIZE, UNIT_SIZE);
-        		
-        	}
-        	else {
-        		g.setColor(new Color(45,180,0));
-        		g.fillRect(x[i],y[i], UNIT_SIZE, UNIT_SIZE);
-        	}
-        }
-        
-        
-        //drawing some lines to make a grid
-        //it will ease our work
-        for(int i=0; i<SCREEN_HEIGHT; i++){
-            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH,i*UNIT_SIZE);
-        }
-        
-    
+    	if(running) {  //if the game is running
+    		g.setColor(Color.red); //sets apple color
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE); //draws the apple
+            
+            //drawing snake body parts
+            for (int i =0 ; i< bodyParts; i++) {
+            	if (i==0) { //snake head
+            		g.setColor(Color.green); //sets head color
+            		g.fillRect(x[i],y[i], UNIT_SIZE, UNIT_SIZE); //draws snake head
+            		
+            	}
+            	else {
+            		g.setColor(new Color(45,180,0)); //sets body color
+            		g.fillRect(x[i],y[i], UNIT_SIZE, UNIT_SIZE); //draws snake body parts
+            	}
+            }
+            
+            //drawing the score
+        	g.setColor(Color.red);
+        	g.setFont( new Font("Ink Free",Font.BOLD,40));
+        	FontMetrics metrics = getFontMetrics(g.getFont()); //helps centralizing the text
+        	//drawing a centralized text in the top
+        	g.drawString("Score: "+ applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+ applesEaten))/2, g.getFont().getSize());
+        	
+        	
+        	//drawing some lines to make a grid
+            //it will ease our work
+        	g.setColor(Color.gray);
+            for(int i=0; i<SCREEN_HEIGHT; i++){
+                g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH,i*UNIT_SIZE);
+            }
+    	}
+    	
+    	else { //if the game is not running
+    		gameOver(g);
+    	}
     }
-    public void newApple(){
+    public void newApple(){ //draw a new apple in a random position
     	appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
     	appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
-
     }
     
     public void move(){
@@ -97,8 +109,12 @@
 	    }
         
     }
-    public void checkApple() {
-
+    public void checkApple() { //checks if snake head has the same position of the apple generated
+    	if((x[0] == appleX) && (y[0] == appleY)) {
+    		bodyParts++; //increments body parts
+    		applesEaten++; //increments apple eaten count
+    		newApple(); //spawns a new apple
+    	}
     }
     public void checkCollisions() {
         //checks if head collides with body
@@ -126,8 +142,22 @@
     	}
     }
     public void gameOver(Graphics g) {
+    	//Writes a game over text
+    	g.setColor(Color.red);
+    	g.setFont( new Font("Ink Free",Font.BOLD,75));
+    	FontMetrics metrics1 = getFontMetrics(g.getFont()); //helps centralizing the text
+    	//drawing a centralized text:
+    	g.drawString("Game Over", (SCREEN_WIDTH - metrics1.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+    	
+    	//drawing the score
+    	g.setFont(new Font("Ink Free",Font.BOLD,40));
+    	FontMetrics metrics2 = getFontMetrics(g.getFont()); //helps centralizing the text
+    	//drawing a centralized text in the top
+    	g.drawString("Score: "+ applesEaten, (SCREEN_WIDTH - metrics2.stringWidth("Score: "+ applesEaten))/2, g.getFont().getSize());
+    
     }
-    @Override
+    
+    //@Override
     public void actionPerformed(ActionEvent e) {
         if(running) { //if the game is running
         	move();
@@ -140,7 +170,30 @@
     public class MyKeyAdapter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e) {
+            switch(e.getKeyCode()) {
+            	case KeyEvent.VK_LEFT: //left arrow
+            		if(direction != 'R') { //if the snake is not facing right
+            			direction = 'L'; //then we can turn left
+            		}
+            		break;
+            	case KeyEvent.VK_RIGHT: //right arrow
+            		if(direction != 'L') { //if the snake is not facing left
+            			direction = 'R'; //then we can turn right
+            		}
+            		break;
+            	case KeyEvent.VK_UP: //up arrow
+            		if(direction != 'D') { //if the snake is not facing down
+            			direction = 'U'; //then we can turn up
+            		}
+            		break;
+            	
+            	case KeyEvent.VK_DOWN: //down arrow
+            		if(direction != 'U') { //if the snake is not facing up
+            			direction = 'D'; //then we can turn down
+            		}
+            		break;
             
+            }
             
         }
     }
